1b7a85929bb7f04c78cfb761e576ff3b7dccaf05
franciscocaballero <caballerofrancisco22@yahoo.com>
2021-06-15 23:02:56 -0400

    adding popups on click events

--- script.js
+++ script.js
@@ -18,6 +18,32 @@ if (navigator.geolocation)
       //Using destructuring: const {latitude} = position.coords
       const { longitude } = position.coords;
       console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+      const coords = [latitude, longitude];
+      const map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot//{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      map.on('click', function (mapEvent) {
+        console.log(mapEvent);
+        const { lat, lng } = mapEvent.latlng;
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minHeight: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+          )
+          .setPopupContent(`workout`)
+          .openPopup();
+        // L.marker([lat, lng]).addTo(map).bindPopup('Workout🏃🏻‍♂️').openPopup();
+      });
     },
     function () {
       alert('Could not get your position');
