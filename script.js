12404ce84d7f8d428927794dd19b7967ebc8a2bb
franciscocaballero <caballerofrancisco22@yahoo.com>
2021-06-21 23:52:26 -0400

    adding work outs

--- script.js
+++ script.js
@@ -100,16 +100,46 @@ class App {
   }
 
   _newWorkout(e) {
+    const validInputs = (...inputs) =>
+      inputs.every(inp => Number.isFinite(inp));
+
+    const allPositive = (...inputs) => inputs.every(inp => inp > 0);
+
     e.preventDefault();
 
-    //Clear inputfields:
-    inputDistance.value =
-      inputDuration.value =
-      inputCadence.value =
-      inputElevation.value =
-        '';
+    //Get data from form
+    const type = inputType.value;
+    const distance = inputDistance.value;
+    const duration = inputDuration.value;
+    // Check if data is valid
 
-    //Display maker:
+    // If activity running, create running object
+    if (type === 'running') {
+      //check if data is valid
+      const cadence = +inputCadence.value;
+      if (
+        // !Number.isFinite(distance) ||
+        // !Number.isFinite(duration) ||
+        // !Number.isFinite(cadence)
+        !validInputs(distance, duration, cadence) ||
+        !allPositive(distance, duration, cadence)
+      )
+        return alert('Inputs have to be positive number');
+    }
+    // If activity cycling, create running object
+    if (type === 'cycling') {
+      const elevation = +inputElevation.value;
+      if (
+        // !Number.isFinite(distance) ||
+        // !Number.isFinite(duration) ||
+        // !Number.isFinite(cadence)
+        !validInputs(distance, duration, elevation)
+      )
+        return alert('Inputs have to be positive number');
+    }
+    // Add new object to workout array
+
+    // Render workout on map as marker
     const { lat, lng } = this.#mapEvent.latlng;
     L.marker([lat, lng])
       .addTo(this.#map)
@@ -124,6 +154,14 @@ class App {
       )
       .setPopupContent(`workout`)
       .openPopup();
+    // Render work workout to list
+
+    // Hide  form + clear input fieids
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        '';
   }
 }
 
